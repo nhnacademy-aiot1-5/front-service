1c318ce1a5a45b75db16ecbe28fafd3d2b26373d
Jeongyong Lee <hua9378@gmail.com>
2024-03-30 14:42:05 +0900

    first commit

---
+++ src/test/java/live/ioteatime/frontservice/FrontServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package live.ioteatime.frontservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
