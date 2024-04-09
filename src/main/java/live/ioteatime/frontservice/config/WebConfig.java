5e32c1a1d5d14f21cbd06078635df2ee72ea6700
july433 <july433@naver.com>
2024-04-09 14:36:21 +0900

    feat : guest page, admin page

--- src/main/java/live/ioteatime/frontservice/config/WebConfig.java
+++ src/main/java/live/ioteatime/frontservice/config/WebConfig.java
@@ -12,7 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/login").setViewName("authentication/login");
         registry.addViewController("/register").setViewName("authentication/register");
-        registry.addViewController("/mypage").setViewName("mypage/mypage");
+        registry.addViewController("/user").setViewName("mypage/user-page");
+        registry.addViewController("/guest").setViewName("mypage/guest-page");
+        registry.addViewController("/admin").setViewName("mypage/admin-page");
     }
 
 }
