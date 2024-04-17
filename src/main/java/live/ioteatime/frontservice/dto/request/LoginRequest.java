01581abb1dbb3c6a8ea519b13b9488a99268d62d
haedanui <rhs9705kr@naver.com>
2024-04-17 21:59:03 +0900

    fix : DTO Response Request 구분함

--- src/main/java/live/ioteatime/frontservice/dto/request/LoginRequest.java
+++ src/main/java/live/ioteatime/frontservice/dto/request/LoginRequest.java
@@ -1,4 +1,4 @@
-package live.ioteatime.frontservice.dto;
+package live.ioteatime.frontservice.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
