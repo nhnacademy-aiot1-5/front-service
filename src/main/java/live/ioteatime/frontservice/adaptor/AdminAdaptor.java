02d7c64bc27295a2148e694d11647043dff553cc
haedanui <rhs9705kr@naver.com>
2024-04-17 14:51:12 +0900

    fix : FeignClientConfig 파일 추가로 중복코드 제거

--- src/main/java/live/ioteatime/frontservice/adaptor/AdminAdaptor.java
+++ src/main/java/live/ioteatime/frontservice/adaptor/AdminAdaptor.java
@@ -2,9 +2,10 @@ package live.ioteatime.frontservice.adaptor;
 
 import live.ioteatime.frontservice.dto.GetUserResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -13,17 +14,17 @@ public interface AdminAdaptor {
 
     //GUEST 권한을 가진 유저 정보 리스트를 가져옴
     @GetMapping("/api/admin/guests")
-    ResponseEntity<List<GetUserResponse>> requestGuestUsers(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader);
+    ResponseEntity<List<GetUserResponse>> requestGuestUsers();
 
     //유저 정보 리스트를 가져옴
     @GetMapping("/api/admin/users")
-    ResponseEntity<List<GetUserResponse>> requestUsers(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader);
+    ResponseEntity<List<GetUserResponse>> requestUsers();
 
     //센서 정보 리스트를 가져옴
     @GetMapping("/api/admin/sensors")
-    ResponseEntity<List<GetUserResponse>> requestSensors(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader);
+    ResponseEntity<List<GetUserResponse>> requestSensors();
 
     //해당하는 유저의 권한을 GUEST에서 USER로 바꿈
     @PutMapping("/api/admin/roles")
-    ResponseEntity<GetUserResponse> requestRole(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestParam String userId);
+    ResponseEntity<GetUserResponse> requestRole(@RequestParam String userId);
 }
--- src/main/java/live/ioteatime/frontservice/controller/AdminController.java
+++ src/main/java/live/ioteatime/frontservice/controller/AdminController.java
@@ -3,7 +3,6 @@ package live.ioteatime.frontservice.controller;
 import live.ioteatime.frontservice.adaptor.AdminAdaptor;
 import live.ioteatime.frontservice.adaptor.UserAdaptor;
 import live.ioteatime.frontservice.dto.GetUserResponse;
-import live.ioteatime.frontservice.utils.CookieUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -13,40 +12,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/admin")
 public class AdminController {
-    private final String ACCESS_TOKEN_KEY = "iotaot";
-    private final String TOKEN_PREFIX = "Bearer ";
     private final AdminAdaptor adminAdaptor;
     private final UserAdaptor userAdaptor;
-    private final CookieUtil cookieUtil;
+
 
     /**
      * 어드민 페이지에 처음 들어왔을때 기본으로 로딩되는 페이지로 GUEST 유저의 리스트를 확인할 수 있는 페이지를 로딩합니다.
-     * @param request 페이지에서 받아온 요청들입니다.
      * @param model 결과 페이지에 전달할 파라미터입니다.
      * @return 해당하는 경로의 html 파일을 불러옵니다.
      */
     @GetMapping
-    public String admin(HttpServletRequest request, Model model) {
-        String accessToken = cookieUtil.getCookieValue(request, ACCESS_TOKEN_KEY);
-        log.info("access token: {}", accessToken);
-        if (Objects.isNull(accessToken)) {
-            return "redirect:/login";
-        }
+    public String admin(Model model) {
+
         //사이드바 전용
-        GetUserResponse userInfo = userAdaptor.getUser(TOKEN_PREFIX + accessToken).getBody();
+        GetUserResponse userInfo = userAdaptor.getUser().getBody();
         model.addAttribute("userInfo", userInfo);
 
         //Guest 유저 관리
-        List<GetUserResponse> userList = adminAdaptor.requestGuestUsers(TOKEN_PREFIX + accessToken).getBody();
+        List<GetUserResponse> userList = adminAdaptor.requestGuestUsers().getBody();
         model.addAttribute("userList", userList);
 
         return "/admin/adminauthority";
@@ -55,29 +45,24 @@ public class AdminController {
     /**
      *
      * GUEST 유저의 리스트를 확인할 수 있는 페이지를 로딩합니다.
-     * @param request 페이지에서 받아온 요청들입니다.
      * @param model 결과 페이지에 전달할 파라미터입니다.
      * @return 해당하는 경로의 html 파일을 불러옵니다.
      */
     @GetMapping("/roleup")
-    public String roleUp(HttpServletRequest request, Model model) {
-        return admin(request, model);
+    public String roleUp(Model model) {
+        return admin(model);
     }
 
     @GetMapping("/users")
-    public String userList(HttpServletRequest request, Model model) {
-        String accessToken = cookieUtil.getCookieValue(request, ACCESS_TOKEN_KEY);
-        log.info("access token: {}", accessToken);
-        if (Objects.isNull(accessToken)) {
-            return "redirect:/login";
-        }
+    public String userList(Model model) {
+
         //사이드바
-        GetUserResponse userInfo = userAdaptor.getUser("Bearer " + accessToken).getBody();
+        GetUserResponse userInfo = userAdaptor.getUser().getBody();
         model.addAttribute("userInfo", userInfo);
         log.info("userId: {}, userName: {}, userRole={}", userInfo.getId(), userInfo.getName(), userInfo.getRole());
 
         //유저 정보
-        List<GetUserResponse> userList = adminAdaptor.requestUsers(TOKEN_PREFIX + accessToken).getBody();
+        List<GetUserResponse> userList = adminAdaptor.requestUsers().getBody();
         model.addAttribute("userList", userList);
 
         return "/admin/adminuser";
@@ -86,25 +71,18 @@ public class AdminController {
 
     /**
      * GUEST 유저의 USER로의 권한 업그레이드 요청을 처리하는 핸들러 메서드입니다.
-     *
-     * @param request 브라우저 http 요청
      * @return 마이페이지로 리다이렉트
      */
     @PutMapping("/roles/{userId}")
-    public String upgradeFromGuestToMember(HttpServletRequest request, @PathVariable("userId") String userId, Model model) {
-        String accessToken = cookieUtil.getCookieValue(request, ACCESS_TOKEN_KEY);
-        log.info("access token: {}", accessToken);
-        if (Objects.isNull(accessToken)) {
-            return "redirect:/login";
-        }
+    public String upgradeFromGuestToMember(@PathVariable("userId") String userId, Model model) {
 
         //사이드바
-        GetUserResponse userInfo = userAdaptor.getUser(TOKEN_PREFIX + accessToken).getBody();
+        GetUserResponse userInfo = userAdaptor.getUser().getBody();
         model.addAttribute("userInfo", userInfo);
         log.info("userId: {}, userName: {}, userRole={}", userInfo.getId(), userInfo.getName(), userInfo.getRole());
 
         //유저정보 변경
-        adminAdaptor.requestRole(TOKEN_PREFIX + accessToken, userId);
+        adminAdaptor.requestRole(userId);
         return "redirect:/admin";
     }
 }
