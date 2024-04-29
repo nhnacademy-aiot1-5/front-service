d6e09e595e8b096da57510e4e7bdcecf8d913efc
haedanui <rhs9705kr@naver.com>
2024-04-29 17:26:37 +0900

    fix : jsonproperty 추가

--- src/main/java/live/ioteatime/frontservice/dto/response/GetModbusSensorResponse.java
+++ src/main/java/live/ioteatime/frontservice/dto/response/GetModbusSensorResponse.java
@@ -15,6 +15,7 @@ public class GetModbusSensorResponse {
     private String modelName;
     private String ip;
     private String port;
+    @JsonProperty("channel_count")
     private int channelCount;
     private Alive alive;
 }
