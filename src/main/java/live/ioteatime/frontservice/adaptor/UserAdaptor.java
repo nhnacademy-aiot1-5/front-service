9215752b3d7682197129d267510d1edff326c7c8
codeTheStudent <hjun303@gnu.ac.kr>
2024-04-29 23:44:57 +0900

    Feat : front 에서 실시간 값 그래프 표현 하는 기능 구현

--- src/main/java/live/ioteatime/frontservice/adaptor/UserAdaptor.java
+++ src/main/java/live/ioteatime/frontservice/adaptor/UserAdaptor.java
@@ -1,7 +1,9 @@
 package live.ioteatime.frontservice.adaptor;
 
+import live.ioteatime.frontservice.dto.ChannelDto;
 import live.ioteatime.frontservice.dto.DailyElectricityDto;
 import live.ioteatime.frontservice.dto.MonthlyElectricityDto;
+import live.ioteatime.frontservice.dto.PlaceDto;
 import live.ioteatime.frontservice.dto.request.ChangePasswordRequest;
 import live.ioteatime.frontservice.dto.request.LoginRequest;
 import live.ioteatime.frontservice.dto.request.RegisterRequest;
@@ -14,7 +16,6 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -53,17 +54,23 @@ public interface UserAdaptor {
     ResponseEntity<String> getMonthlyElectricity(@RequestParam
                                                  @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
                                                  LocalDateTime localDateTime,
-                                                 @RequestParam int organizationId);
+                                                 @RequestParam int channelId);
 
     @GetMapping("/api/daily/electricities")
     ResponseEntity<List<DailyElectricityDto>> getDailyElectricities(@RequestParam
                                                                     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
                                                                     LocalDateTime localDateTime,
-                                                                    @RequestParam int organizationId);
+                                                                    @RequestParam int channelId);
 
     @GetMapping("/api/monthly/electricities")
     ResponseEntity<List<MonthlyElectricityDto>> getMonthlyElectricities(@RequestParam
                                                                         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
                                                                         LocalDateTime localDateTime,
-                                                                        @RequestParam int organizationId);
+                                                                        @RequestParam int channelId);
+
+    @GetMapping("/api/places")
+    ResponseEntity<List<PlaceDto>> getPlacesByOrganizationId(@RequestParam int organizationId);
+
+    @GetMapping("/api/channels/by_place")
+    ResponseEntity<List<ChannelDto>> getChannelsByPlaceId(@RequestParam int placeId);
 }
---
+++ src/main/java/live/ioteatime/frontservice/dto/PlaceDto.java
@@ -0,0 +1,13 @@
+package live.ioteatime.frontservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlaceDto {
+    private int id;
+    private String placeName;
+}
