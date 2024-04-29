a406ecbc2954e97bee504dda48bbcaf51b5aac40
haedanui <rhs9705kr@naver.com>
2024-04-29 17:17:11 +0900

    feat : 센서 페이지 작성

---
+++ src/main/java/live/ioteatime/frontservice/dto/response/GetModbusSensorResponse.java
@@ -0,0 +1,20 @@
+package live.ioteatime.frontservice.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import live.ioteatime.frontservice.domain.Alive;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class GetModbusSensorResponse {
+    private int id;
+    @JsonProperty("sensor_name")
+    private String name;
+    @JsonProperty("sensor_model_name")
+    private String modelName;
+    private String ip;
+    private String port;
+    private int channelCount;
+    private Alive alive;
+}
