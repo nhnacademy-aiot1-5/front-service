238dd42198f7ba0f8f8b23f9f87f63b963b7ffd8
haedanui <rhs9705kr@naver.com>
2024-05-10 16:29:13 +0900

    fix : DTO 양식 수정

--- src/main/java/live/ioteatime/frontservice/dto/PlaceDto.java
+++ src/main/java/live/ioteatime/frontservice/dto/PlaceDto.java
@@ -1,16 +1,20 @@
 package live.ioteatime.frontservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import live.ioteatime.frontservice.dto.response.OrganizationResponse;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
 @NoArgsConstructor
-@AllArgsConstructor
+@Getter
+@Setter
 public class PlaceDto {
     @JsonProperty("place_id")
     private int id;
     @JsonProperty("place_name")
     private String placeName;
+    @JsonProperty("organization_id")
+    private int organizationId;
+    private OrganizationResponse organization;
 }
