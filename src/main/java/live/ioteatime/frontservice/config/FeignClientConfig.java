6978f4f54825fa75bca67c7b9651636668065f6b
sylim <sammys27@naver.com>
2024-05-17 19:22:17 +0900

    refactor : 로그인, 회원가입 시에만 액세스 토큰 체크하지 않도록 변경

--- src/main/java/live/ioteatime/frontservice/config/FeignClientConfig.java
+++ src/main/java/live/ioteatime/frontservice/config/FeignClientConfig.java
@@ -20,9 +20,10 @@ import javax.servlet.http.HttpServletRequest;
 public class FeignClientConfig {
 
     /**
+     * OpenFeign으로 게이트웨이에 요청을 보내는 경우, 액세스 토큰을 요청 헤더에 저장하기 위한 인터셉터입니다.
      * 로그인 하지 않은 유저가 엑세스 토큰 없이 회원 가입 페이지와 로그인 페이지에 접속할 수 있도록 하고,
      * 회원 가입과 로그인을 제외한 요청이 들어올 경우 엑세스 토큰이 없다면 UnauthorizedAccessException을 리턴하고,
-     * 토큰이 있다면 헤더에 토큰 정보를 저장 합니다.
+     * 브라우저 쿠키에 토큰이 있다면 요청 헤더에 토큰 정보를 저장 합니다.
      *
      * @param cookieUtil 엑세스 토큰을 가져오기위해 사용 됩니다.
      * @return 로그인이나 회원 가입 요청이라면 해당 페이지를, 그 외의 요청에서 토큰이 없는 경우 에러를 던집니다.
@@ -30,14 +31,11 @@ public class FeignClientConfig {
     @Bean
     public RequestInterceptor requestTokenBearerInterceptor(CookieUtil cookieUtil) {
         return requestTemplate -> {
-
             HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
 
             String url = requestTemplate.url();
             log.error("{} {}", url, request.getMethod());
-            if (url.equals("/auth/login") ||
-                    (request.getMethod().equals("GET") && (url.equals("/users/update-user") || url.equals("/users/change-password"))) ||
-                    (url.equals("/api/users") && request.getMethod().equals("POST"))
+            if (url.equals("/auth/login") || (url.equals("/api/users") && request.getMethod().equals("POST"))
             ) {
                 return;
             }
@@ -47,8 +45,6 @@ public class FeignClientConfig {
             }
             requestTemplate.header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
             log.info("AccessToken applied for request: {}", requestTemplate.headers().get("Authorization"));
-
         };
     }
 }
-
