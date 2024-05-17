da1619f86dcab6ba3779b751b63570f03facafe5
sylim <sammys27@naver.com>
2024-05-17 21:23:40 +0900

    add : dto에 `@JsonFormat` 추가

--- src/main/java/live/ioteatime/frontservice/dto/response/BudgetHistoryResponse.java
+++ src/main/java/live/ioteatime/frontservice/dto/response/BudgetHistoryResponse.java
@@ -1,5 +1,6 @@
 package live.ioteatime.frontservice.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 public class BudgetHistoryResponse {
     private int id;
     @JsonProperty("change_time")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime changeTime;
     private long budget;
 }
