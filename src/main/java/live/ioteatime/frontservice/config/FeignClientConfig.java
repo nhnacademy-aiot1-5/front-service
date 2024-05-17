67865c061d11514975171aa3bcb95674c7fffac6
dmswl <siddltkfkd@gmail.com>
2024-05-17 16:00:58 +0900

    docs: Config에 자바독 추가

--- src/main/java/live/ioteatime/frontservice/config/FeignClientConfig.java
+++ src/main/java/live/ioteatime/frontservice/config/FeignClientConfig.java
@@ -12,10 +12,21 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * FeignClient에 관한 설정을 처리하는 클래스 입니다.
+ */
 @Slf4j
 @Configuration
 public class FeignClientConfig {
 
+    /**
+     * 로그인 하지 않은 유저가 엑세스 토큰 없이 회원 가입 페이지와 로그인 페이지에 접속할 수 있도록 하고,
+     * 회원 가입과 로그인을 제외한 요청이 들어올 경우 엑세스 토큰이 없다면 UnauthorizedAccessException을 리턴하고,
+     * 토큰이 있다면 헤더에 토큰 정보를 저장 합니다.
+     *
+     * @param cookieUtil 엑세스 토큰을 가져오기위해 사용 됩니다.
+     * @return 로그인이나 회원 가입 요청이라면 해당 페이지를, 그 외의 요청에서 토큰이 없는 경우 에러를 던집니다.
+     */
     @Bean
     public RequestInterceptor requestTokenBearerInterceptor(CookieUtil cookieUtil) {
         return requestTemplate -> {
