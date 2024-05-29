b2da4e1e96557e0243f9974ba0eb0fc38b76bfe7
codeTheStudent <hjun303@gnu.ac.kr>
2024-05-29 20:47:24 +0900

    fix : sse header X-Accel-Buffering 설정
    SSE 응답을 반환하는 API의 헤더에 X-Accel-Buffering: no를 붙여 SSE 응답만 버퍼링을 하지 않도록 설정

--- src/main/java/live/ioteatime/frontservice/controller/SseController.java
+++ src/main/java/live/ioteatime/frontservice/controller/SseController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,7 +27,7 @@ public class SseController {
     private final Map<Integer, Map<String, SseEmitter>> emitters = new ConcurrentHashMap<>();
 
     @GetMapping("/sse")
-    public SseEmitter subscribeSse(HttpServletRequest request) {
+    public SseEmitter subscribeSse(HttpServletRequest request, HttpServletResponse response) {
         log.info("client subscribed : {}", request.getRemoteAddr());
         GetUserResponse userInfo = (GetUserResponse) request.getAttribute("userInfo");
         int orgId = userInfo.getOrganization().getId();
@@ -44,12 +45,12 @@ public class SseController {
             this.emitters.get(orgId).remove(userId);
             log.error("SSE connection error for user: {}", userId, e);
         });
-
+        response.addHeader("X-Accel-Buffering", "no");
         return emitter;
     }
 
     @PostMapping("/notify")
-    public void notifyClients(@RequestBody Alert alert) {
+    public void notifyClients(@RequestBody Alert alert, HttpServletResponse response) {
         log.info("outlier notify called : {}", alert);
         int targetOrgId = alert.getOrganizationId();
         List<String> toRemove = new ArrayList<>();
@@ -58,6 +59,7 @@ public class SseController {
         for (Map.Entry<String, SseEmitter> entry : targetEmitters.entrySet()) {
             SseEmitter emitter = entry.getValue();
             try {
+                response.addHeader("X-Accel-Buffering", "no");
                 emitter.send(SseEmitter.event().data(sseService.getMessage(alert)));
             } catch (IOException e) {
                 toRemove.add(entry.getKey());
